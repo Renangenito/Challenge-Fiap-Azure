293024cdff051495be7e53a1ae688297e2dd80cd
Renan Lima <renangenito@hotmail.com>
2023-07-05 10:48:19 -0300

    Apagando comentários

--- ChallengeFiap.API/Controllers/ClienteController.cs
+++ ChallengeFiap.API/Controllers/ClienteController.cs
@@ -57,33 +57,5 @@ namespace ChallengeFiap.API.Controllers
             await _cliente.Incluir(clienteModel);
         }
 
-        //public async Task<byte[]> GetImageFromBlobStorage(ClienteModel clienteModel)
-        //{
-        //    if (clienteModel.Imagem != null && clienteModel.Imagem.Length > 0)
-        //    {
-        //        // Criar um objeto BlobServiceClient usando a string de conexão
-        //        var blobServiceClient = new BlobServiceClient("DefaultEndpointsProtocol=https;AccountName=renanimg01;AccountKey=hKxktZcGt+LWR3Bv9mHh576lhyZP9vrceBQklTRMvXcWb7xNLgAT0oHg6/RTPE1syTD++EddwIB5+AStViZINQ==;EndpointSuffix=core.windows.net");
-
-        //        // Obter uma referência para o contêiner de blobs desejado
-        //        var containerClient = blobServiceClient.GetBlobContainerClient("dados");
-
-        //        // Obter uma referência para o blob desejado
-        //        var blobClient = containerClient.GetBlobClient(clienteModel.Imagem);
-
-        //        // Verificar se o blob existe
-        //        if (await blobClient.ExistsAsync())
-        //        {
-        //            // Fazer o download do blob para um MemoryStream
-        //            using (var memoryStream = new MemoryStream())
-        //            {
-        //                await blobClient.DownloadToAsync(memoryStream);
-        //                return memoryStream.ToArray();
-        //            }
-        //        }
-        //    }
-
-        //    // Se o blob não existir ou se as propriedades necessárias não forem fornecidas, você pode lidar com isso de acordo com sua lógica de negócio
-        //    return null;
-        //}
     }
 }
